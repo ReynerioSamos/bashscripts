5caca3b37aa50718b80c0b43caa9bef5a45c6042
ReynerioSamos <Reynerio.Samos@gmail.com>
2024-09-29 17:49:07 -0600

    updated result prompts for readability

--- operations.sh
+++ operations.sh
@@ -9,30 +9,30 @@ read -r num2
 echo -n "Enter operation [add,sub,mult,div]: "
 read -r oper
 
-echo -n "$oper operation..."
+echo -n "$oper operation...\n"
 case $oper in
 
     "add")
     res=$((num1+num2))
-    echo -n "$num1 + $num2 = $res"
+    echo -n "$num1 + $num2 = $res\n"
     ;;
 
     "sub")
     res=$((num1-num2))
-    echo -n "$num1 - $num2 = $res"
+    echo -n "$num1 - $num2 = $res\n"
     ;;
 
     "mult")
     res=$((num1*num2))
-    echo -n "$num1 x $num2 = $res"
+    echo -n "$num1 x $num2 = $res\n"
     ;;
 
     "div")
     res=$((num1/num2))
-    echo -n "$num1 / $num2 = $res"
+    echo -n "$num1 / $num2 = $res\n"
     ;;
 
     *)
-    echo -n "Invalid"
+    echo -n "Please choose a correct operator.\n"
     ;;
 esac
