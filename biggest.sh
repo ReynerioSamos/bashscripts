16780dc07c73e8c9aa7d4c04094ca3050024c1ac
ReynerioSamos <Reynerio.Samos@gmail.com>
2024-09-29 16:54:39 -0600

    changed conditionals

--- biggest.sh
+++ biggest.sh
@@ -8,14 +8,14 @@ NUM3="$((7))"
 
 echo "Script to find biggest number from : $NUM1 , $NUM2, $NUM3"
 
-if [ "$NUM1" -gt "$NUM2" ] 
-then
-	if [ "$NUM1" -gt "$NUM3" ]
-	then echo "$NUM1 is the biggest number"
-	fi
-elif [ "$NUM2" -gt "$NUM3" ] 
-then echo "$NUM2 is the biggest number"
-else echo "$NUM3 is the biggest number"
+if [ "$NUM1" -gt "$NUM2" ] && [ "$NUM1" -gt "$NUM3" ]
+then 
+	echo "$NUM1 is the biggest number"
+elif [ "$NUM2" -gt "$NUM1" ] && [ "$NUM2" -gt "$NUM3"]
+then 
+	echo "$NUM2 is the biggest number"
+else 
+	echo "$NUM3 is the biggest number"
 
 fi
 
