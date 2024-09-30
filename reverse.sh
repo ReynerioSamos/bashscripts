e35db12cd89345ab9258710a250a81c5b95306d7
ReynerioSamos <Reynerio.Samos@gmail.com>
2024-09-29 19:31:57 -0600

    added reverse.sh

---
+++ reverse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 6) Script to reverse a given number - while block
+
+echo -n "Enter a number to be reversed: "
+read -r num
+
+strarr=("$num")
+length=${#num}
+i="$((0))"
+
+echo -n "$num reversed is: "
+while [ "$i" -lt "$length-1" ]
+    do
+        echo "${strarr[$i]}"
+        ((i++))
+    done
