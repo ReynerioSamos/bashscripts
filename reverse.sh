e9fdcab98e206cd74b3cbfdd59e826d9ecbc4b69
ReynerioSamos <Reynerio.Samos@gmail.com>
2024-09-30 02:06:26 +0000

    edited split method and chmod u+x perm

--- reverse.sh
+++ reverse.sh
@@ -5,12 +5,13 @@
 echo -n "Enter a number to be reversed: "
 read -r num
 
-strarr="$(fold -w1 <<< "$num")"
+mapfile -t strarr < <(fold -w1 <<< "$num")
 end=${#num}
 
 echo -n "$num reversed is: "
 while [ "$end" -gt "$((0))" ]
     do
-        echo "${strarr[$(($end-1))]}"
+        echo -n "${strarr[$(($end-1))]}"
         ((end--))
     done
+echo
