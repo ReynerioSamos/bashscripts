2e958363f71bc2a27be32001d1d702b4b75211ff
ReynerioSamos <Reynerio.Samos@gmail.com>
2024-09-29 19:06:12 -0600

    edited to allow user input

--- biggest.sh
+++ biggest.sh
@@ -2,20 +2,24 @@
 
 # 4) Script: Find biggest number in 3 numbers - if -elif block
 
-NUM1="$((3))"
-NUM2="$((1))"
-NUM3="$((7))"
+echo -n "Enter first number: "
+read -r NUM1
+echo -n "Enter second number: "
+read -r NUM2
+echo -n "Enter third number: "
+read -r NUM3
 
 echo "Script to find biggest number from : $NUM1 , $NUM2, $NUM3"
 
 if [ "$NUM1" -gt "$NUM2" ] && [ "$NUM1" -gt "$NUM3" ]
 then 
-	echo "$NUM1 is the biggest number"
+	echo -ne "$NUM1 is the biggest number\n"
+
 elif [ "$NUM2" -gt "$NUM1" ] && [ "$NUM2" -gt "$NUM3" ]
 then 
-	echo "$NUM2 is the biggest number"
-else 
-	echo "$NUM3 is the biggest number"
+	echo -ne "$NUM2 is the biggest number\n"
 
+else 
+	echo -ne "$NUM3 is the biggest number\n"
 fi
 
